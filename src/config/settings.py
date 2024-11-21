08654b59d0d80971af8397e2184aca6f83a2454d
gaxeris <andreykardanev@gmail.com>
2024-11-21 20:01:14 +0300

    feature: add main taskmanagementsystem app

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.users.apps.UsersConfig",
+    "apps.task_management_system.apps.TaskManagementSystemConfig",
 ]
 
 MIDDLEWARE = [
