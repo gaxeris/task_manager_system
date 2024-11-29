901f5cab81086c2d04a73e8ccc87708cd84d445a
gaxeris <andreykardanev@gmail.com>
2024-11-29 21:04:18 +0300

    feature: register project and task models to make them available in admin

--- src/apps/task_management_system/admin.py
+++ src/apps/task_management_system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from apps.task_management_system.models import Project, Task
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
