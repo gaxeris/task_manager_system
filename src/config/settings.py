8c1bd1e7ae50266a0a1f04a8bbcbf4de18e9a251
gaxeris <andreykardanev@gmail.com>
2024-12-03 13:10:21 +0300

    dependencies: configure celery

--- src/config/settings.py
+++ src/config/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Celery settings
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://localhost:6379/0")
