65df6d13443fca3ea7998589ab529b97830b3b4f
gaxeris <andreykardanev@gmail.com>
2024-12-06 21:32:46 +0300

    feature: modify serializers to display both ForeignKey id and name of its relating value
    Projects FK values are not meant to be modified by POST or other methods so they use rad_only nested serializer fields.
    For Tasks FK it was decided to keep models FK in ModelSerializers unmodified, adding extra string values instead to provide more readable result

--- src/apps/task_management_system/serializers.py
+++ src/apps/task_management_system/serializers.py
@@ -1,14 +1,39 @@
 from rest_framework import serializers
 from .models import Project, Task
+from django.contrib.auth import get_user_model
+
+
+class UserShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username"]
+
+
+class TaskShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["id", "title", "priority", "status"]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+
+    tasks = TaskShortSerializer(many=True, read_only=True)
+    created_by = UserShortSerializer(read_only=True)
+
     class Meta:
         model = Project
-        fields = ["id", "name", "description", "created_by", "created_at"]
+        fields = ["id", "name", "description", "tasks", "created_by", "created_at"]
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    project_str = serializers.StringRelatedField(source="project.name", read_only=True)
+    created_by_str = serializers.StringRelatedField(
+        source="created_by.username", read_only=True
+    )
+    assigned_to_str = serializers.StringRelatedField(
+        source="assigned_to.username", read_only=True
+    )
+
     class Meta:
         model = Task
         fields = [
@@ -19,7 +44,13 @@ class TaskSerializer(serializers.ModelSerializer):
             "status",
             "deadline",
             "project",
+            "project_str",
             "assigned_to",
+            "assigned_to_str",
             "created_by",
+            "created_by_str",
             "created_at",
+        ]
+        read_only_fields = [
+            "created_by",
         ]
