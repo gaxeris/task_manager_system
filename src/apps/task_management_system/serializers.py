90e5c7a445838c8883f81e6f6b1286058a97d5e1
gaxeris <andreykardanev@gmail.com>
2024-12-06 21:07:30 +0300

    feature: make nested serializer to present relations accessible by ForeignKey

--- src/apps/task_management_system/serializers.py
+++ src/apps/task_management_system/serializers.py
@@ -1,14 +1,42 @@
 from rest_framework import serializers
 from .models import Project, Task
+from django.contrib.auth import get_user_model
+
+
+class UserShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username"]
+
+
+class TaskShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["id", "title", "priority", "status"]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+
+    tasks = TaskShortSerializer(many=True, read_only=True)
+    created_by = UserShortSerializer()
+
     class Meta:
         model = Project
-        fields = ["id", "name", "description", "created_by", "created_at"]
+        fields = ["id", "name", "description", "tasks", "created_by", "created_at"]
+
+
+class ProjectShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ["id", "name"]
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    # project_name = serializers.StringRelatedField(source="project.name", read_only=True)
+    project = ProjectShortSerializer()
+    created_by = UserShortSerializer(read_only=True)
+    assigned_to = UserShortSerializer()
+
     class Meta:
         model = Task
         fields = [
@@ -19,7 +47,9 @@ class TaskSerializer(serializers.ModelSerializer):
             "status",
             "deadline",
             "project",
+            #  "project_name",
             "assigned_to",
             "created_by",
             "created_at",
         ]
+        # extra_kwargs = {"creeated_by": {"read_only": True}}
