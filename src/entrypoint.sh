8973ac7c517f0995b45aadba1bb00b2b394c6130
gaxeris <andreykardanev@gmail.com>
2024-12-09 19:58:15 +0300

    fix: add brackets to variables, change line end sequence to LF for better compatibility with Unix systems which docker container is based upon

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,7 +5,7 @@ poetry run python manage.py makemigrations --no-input
 poetry run python manage.py migrate --no-input
 poetry run python manage.py collectstatic --no-input
 
-DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL poetry run python manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME} DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD} DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL} poetry run python manage.py createsuperuser --noinput
 
 #without this check celery throws a psycopg2 error despite connecting to Redis container
 echo "Waiting for PostgreSQL"
