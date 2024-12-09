2405bd61cd6721d3553bef662bf2b503f5c14bf9
gaxeris <andreykardanev@gmail.com>
2024-12-09 22:52:38 +0300

    feature: filter in admin

--- src/apps/task_management_system/admin.py
+++ src/apps/task_management_system/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from apps.task_management_system.models import Project, Task
 
+
+class TaskAdmin(admin.ModelAdmin):
+
+    list_filter = [
+        "priority",
+        "status",
+        "deadline",
+    ]
+
+
 # Register your models here.
 admin.site.register(Project)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
