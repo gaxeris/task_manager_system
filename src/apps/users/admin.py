b8d2d655ffa332a73bd950465fa9799d59f2e51b
gaxeris <andreykardanev@gmail.com>
2024-12-12 19:38:15 +0300

    feature: make required email field in user appear in CreationForm

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import AdminUserCreationForm
+from django.forms import EmailField
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserCreationForm(AdminUserCreationForm):
+
+    class Meta(AdminUserCreationForm.Meta):
+        model = User
+        fields = AdminUserCreationForm.Meta.fields + ("email",)
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": ("username", "email", "password1", "password2"),
+                "classes": ("wide",),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
